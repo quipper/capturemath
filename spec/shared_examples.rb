1acf6e692a9d53d5c78ed48088e846d971cc89ee
Ashish Sehra <asehra@gmail.com>
2013-12-10 16:52:18 +0000

    Added exception handling for errors caused by no math ML in request. Also corrected regex for capturing previous errors.

--- spec/shared_examples.rb
+++ spec/shared_examples.rb
@@ -14,6 +14,11 @@ shared_examples_for "it handles server errors" do
     lambda { perform_capture }.should raise_error(Capturemath::Error)
   end
 
+  it 'should raise error when input does not have math' do
+    mock_error(format, :no_math)
+    lambda { perform_capture }.should raise_error(Capturemath::Error, /Invalid MathML/)
+  end
+
   it 'should raise error when server is not reachable' do
     mock_error(format, :connection_refused) 
     lambda { perform_capture }.should raise_error(Errno::ECONNREFUSED)
